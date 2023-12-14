2d102aa1e7a544561c85cc721b6327a8b3ddbc93
Matt_Wu <149099669+potsunwu@users.noreply.github.com>
2023-12-14 18:53:05 +0800

    Update eval_odom.py

--- eval_odom.py
+++ eval_odom.py
@@ -19,7 +19,7 @@ parser.add_argument('--seqs',
 args = parser.parse_args()
 
 eval_tool = KittiEvalOdom()
-gt_dir = "dataset/gt_poses/"
+gt_dir = "dataset/kitti_gt_pose/"
 result_dir = args.result
 
 continue_flag = input("Evaluate result in {}? [y/n]".format(result_dir))
